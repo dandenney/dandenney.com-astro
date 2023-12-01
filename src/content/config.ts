bf906194337b2e7110f7eb2ca03f864c1db7b832
Dan <dd.subscriptions@gmail.com>
2023-11-30 22:08:04 -0600

    feat(no-reserv-ai-tions): add landing page and reviews

--- src/content/config.ts
+++ src/content/config.ts
@@ -30,4 +30,22 @@ const tinkerings = defineCollection({
   }),
 });
 
-export const collections = { blips, posts, tinkerings };
+const reviews = defineCollection({
+  schema: z.object({
+    city: z.string(),
+    country: z.string(),
+    heroImage: z.string().optional(),
+    heroImageAlt: z.string().optional(),
+    heroImageAttribution: z.string().optional(),
+    heroImageUrl: z.string().optional(),
+    state: z.string().optional(),
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = { blips, posts, reviews, tinkerings };
