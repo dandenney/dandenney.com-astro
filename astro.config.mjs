4b9b56e864647bd3a23c6f23a2b5c2650729bf66
Dan Denney <dd.subscriptions@gmail.com>
2024-07-08 22:17:53 -0400

    fix(astro): site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://www.dandenney.com",
   markdown: { shikiConfig: { theme: "nord" } },
   integrations: [mdx(), sitemap(), tailwind()],
 });
